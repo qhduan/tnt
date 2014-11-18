33ea9fcab3dfed62ec2251360c1e49a0d2952204
qhduan <mail@qhduan.com>
2014-11-19 06:52:44 +0800

    fix

--- tnt.js
+++ tnt.js
@@ -16,33 +16,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/manga"));
 
 
-function ListManga (path) {
-  var all = [];
-  var list = fs.readdirSync(path);
-  
-  list.forEach(function (mangaName) {
-    var mangaRoot = path + "/" + mangaName;
-    if (fs.statSync(mangaRoot).isDirectory() == false) return;
-    var manga = {};
-    fs.readdirSync(mangaRoot).forEach(function (volName) {
-      if (fs.statSync(mangaRoot + "/" + volName).isDirectory() == false) return; 
-      manga[volName] = [];
-      fs.readdirSync(mangaRoot + "/" + volName).forEach(function (imageName) {
-        if (imageName.match(/\.jpg|\.jpeg|\.png|\.gif|\.bmp/i)) manga[volName].push(imageName);
-      });
-    });
-    fs.writeFileSync(mangaRoot + "/manga.json", JSON.stringify(manga, null, 4));
-    all.push({
-      name: mangaName,
-      count: Object.keys(manga).length
-    });
-  });
-  fs.writeFileSync(path + "/manga.json", JSON.stringify(all, null, 4));
-  console.log("Manga json generated");
-}
-
-ListManga(__dirname + "/manga");
-
 port = 4000;
 
 app.listen(port, function () {
